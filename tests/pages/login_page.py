05456a166deb92b5497c5aea0693c1891f89115d
Atulb <abharadwaj@degreed.com>
2025-11-26 14:45:46 +0530

    dfdf

--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -5,6 +5,7 @@ class LoginPage:
         self.email_text_box = "#username"
         self.password_text_box = "#password"
         self.sign_in_button = "//input[@value='Sign In']"
+        self.registerButton = "#register"
 
     def login(self, username, password):
         """Login to the application using provided credentials"""
@@ -25,21 +26,11 @@ class LoginPage:
         except Exception as e:
             self.logger.error(f"Error during login page interaction: {str(e)}")
             raise
-    def new_login(self, username, password):
+    def click_register(self, username, password):
         """Login to the application using provided credentials"""
         try:
             self.logger.info("Attempting to fill username field")
-            self.page.locator(self.email_text_box).click()
-            self.page.locator(self.email_text_box).fill(username)
-            self.logger.debug(f"Username field filled with: {username}")
-
-            self.logger.info("Attempting to fill password field")
-            self.page.locator(self.password_text_box).click()
-            self.page.locator(self.password_text_box).fill(password)
-            self.logger.debug("Password field filled")
-
-            self.logger.info("Clicking sign in button")
-            self.page.locator(self.sign_in_button).click()
+            self.page.locator(self.registerButton).click()
             self.logger.info("Sign in button clicked successfully")
         except Exception as e:
             self.logger.error(f"Error during login page interaction: {str(e)}")
