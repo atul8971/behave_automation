207002436dea4bd10e9c669f67786d3916163588
Atulb <abharadwaj@degreed.com>
2025-11-26 18:03:44 +0530

    added new tc

--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -25,3 +25,23 @@ class LoginPage:
         except Exception as e:
             self.logger.error(f"Error during login page interaction: {str(e)}")
             raise
+
+    def register(self, username, password):
+        """Login to the application using provided credentials"""
+        try:
+            self.logger.info("Attempting to fill username field")
+            self.page.locator(self.email_text_box).click()
+            self.page.locator(self.email_text_box).fill(username)
+            self.logger.debug(f"Username field filled with: {username}")
+
+            self.logger.info("Attempting to fill password field")
+            self.page.locator(self.password_text_box).click()
+            self.page.locator(self.password_text_box).fill(password)
+            self.logger.debug("Password field filled")
+
+            self.logger.info("Clicking sign in button")
+            self.page.locator(self.sign_in_button).click()
+            self.logger.info("Sign in button clicked successfully")
+        except Exception as e:
+            self.logger.error(f"Error during login page interaction: {str(e)}")
+            raise
--- tests/steps/login_steps.py
+++ tests/steps/login_steps.py
@@ -5,4 +5,9 @@ from tests.actions.login_actions import LoginActions
 def login_to_application(context, username, password):
     LoginActions(context=context).perform_login(username=username,
                                                   password=password)
+
+@then(u'Loign to application using "{username}" and "{password}"')
+def register_to_application(context, username, password):
+    LoginActions(context=context).perform_login(username=username,
+                                                  password=password)
     
